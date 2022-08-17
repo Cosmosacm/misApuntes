875c94e68a58d842949fa09ec1c2715ea52f6eff
Ardanys Canchila <ardanyscm@yahoo.es>
2022-08-17 13:34:19 -0500

    Creando repositorio de los apuntes

---
+++ CyberSecurity/PowerShell/exercices/practice1.ps1
@@ -0,0 +1,11 @@
+﻿Get-Command -CommandType Cmdlet | Measure-Object
+Get-ChildItem
+Get-Help Get-ChildItem
+Get-Help Get-ChildItem -Detailed
+Get-Help Get-ChildItem -Examples
+Get-Help Get-ChildItem -Full
+Get-Help -Online Get-ChildItem
+Update-Help -Module Microsoft.PowerShell*
+
+Get-ExecutionPolicy
+Set-ExecutionPolicy
---
+++ CyberSecurity/PowerShell/exercices/practice2.ps1
@@ -0,0 +1,15 @@
+﻿Write-Host 'Primera linea de escritura'
+Write-Host 'Aprendiendo Power shell'
+
+$nombre1 = 'Ardanys Canchila M'
+New-Variable nombre2
+New-Variable -Name nombre3
+New-Variable -Name nombre4 -Value 'cosmosacm'
+
+New-Variable -Name constante -Value 'acm' -Option ReadOnly
+
+Get-Variable
+
+$nombre5 = Read-Host 'Digite su nombre'
+
+Write-Host $nombre5.GetType().Name
